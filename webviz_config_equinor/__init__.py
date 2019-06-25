9d2727bcc2e2f70026b3fb5967d17b1599a69873
Anders Fredrik Kiær <akia@statoil.com>
2019-06-25 15:23:02 +0200

    Add plotly settings following Equinor brand

--- webviz_config_equinor/__init__.py
+++ webviz_config_equinor/__init__.py
@@ -25,3 +25,24 @@ equinor_theme.assets = glob.glob(os.path.join(
     'assets',
     '*')
 )
+
+equinor_theme.plotly_layout = {
+    'font': {'family': 'Equinor'},
+    'hoverlabel': {'font': {'family': 'Equinor'}},
+    'colorway': ['#007079',
+                 '#66737d',
+                 '#7d0023',
+                 '#4c9ba1',
+                 '#a44c65',
+                 '#80b7bc',
+                 '#ff1243',
+                 '#919ba2',
+                 '#be8091',
+                 '#b2d4d7',
+                 '#ff597b',
+                 '#bdc3c7',
+                 '#d8b2bd',
+                 '#ffe7d6',
+                 '#d5eaf4',
+                 '#ff88a1']
+    }
