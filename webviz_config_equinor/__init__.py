903e8db6f3a482644492e0269f543cf3fa755243
Anders Fredrik Kiær <31612826+anders-kiaer@users.noreply.github.com>
2019-09-24 10:41:20 +0200

    Use Black (#12)

--- webviz_config_equinor/__init__.py
+++ webviz_config_equinor/__init__.py
@@ -9,19 +9,21 @@ except DistributionNotFound:
     # package is not installed
     pass
 
-equinor_theme = WebvizConfigTheme(theme_name='equinor')
+equinor_theme = WebvizConfigTheme(theme_name="equinor")
 
-equinor_theme.external_stylesheets = \
-    ['https://webviz-cdn.azureedge.net/fonts/index.css',
-     'https://webviz-cdn.azureedge.net/theme/equinor.theme.css']
+equinor_theme.external_stylesheets = [
+    "https://webviz-cdn.azureedge.net/fonts/index.css",
+    "https://webviz-cdn.azureedge.net/theme/equinor.theme.css",
+]
 
 equinor_theme.adjust_csp(
-    {'style-src': ['https://webviz-cdn.azureedge.net'],
-     'img-src':  ['https://sibwebvizcdn.blob.core.windows.net'],
-     'font-src': ['https://webviz-cdn.azureedge.net']})
+    {
+        "style-src": ["https://webviz-cdn.azureedge.net"],
+        "img-src": ["https://sibwebvizcdn.blob.core.windows.net"],
+        "font-src": ["https://webviz-cdn.azureedge.net"],
+    }
+)
 
-equinor_theme.assets = glob.glob(os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    'assets',
-    '*')
+equinor_theme.assets = glob.glob(
+    os.path.join(os.path.dirname(os.path.abspath(__file__)), "assets", "*")
 )
